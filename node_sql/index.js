2c7e8bba461bf46204c495adca8ed27236b560c7
Davifxm <davi1103felix@gmail.com>
2026-04-09 23:02:34 -0300

    feat: implementando funcoes crud

--- node_sql/index.js
+++ node_sql/index.js
@@ -5,66 +5,85 @@ const path = require('path');
 
 const app = express();
 
-app.use(
-    express.urlencoded({
-        extended: true
-    })
-)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.json())
-
-// Configuração do mecanismo de visualização handlebars
+// Configuração Handlebars
 app.engine('handlebars', exphbs.engine());
 app.set('view engine', 'handlebars');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
 
-// Rota para renderizar o arquivo home.handlebars
+// CONFIGURAÇÃO DA CONEXÃO 
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '', 
+  database: 'teste34',
+});
+
+// 1. HOME
 app.get('/', (req, res) => {
   res.render('layouts/home');
 });
 
-
+// 2. INSERIR
 app.post('/teste/insertteste', (req, res) => {
-    const nome = req.body.nome
-    const email = req.body.email
-        const senha = req.body.senha
-
-    const sql = ` INSERT INTO teste (nome, email, senha) VALUES  ('${nome}', '${email}', '${senha}') `
+    const { nome, email, senha } = req.body;
+    const sql = `INSERT INTO teste (nome, email, senha) VALUES ('${nome}', '${email}', '${senha}')`;
+    
     conn.query(sql, function(err){
-        if (err) {
-            console.log(err);
-          }
-          res.redirect('/teste')
-    })
-})
+        if (err) { console.log(err); return; }
+        res.redirect('/teste');
+    });
+});
 
+// 3. LISTAR
 app.get('/teste', (req, res)=> {
-  const sql= "SELECT * FROM teste"
-
-  conn.query(sql, function  (err, data){
-    if (err) {
-      console.log(err);
-     return
-    }
-    const teste =data;
-    console.log(teste);
-
-    res.render('layouts/teste', {teste} );
+  const sql = "SELECT * FROM teste";
+  conn.query(sql, function(err, data){
+    if (err) { console.log(err); return; }
+    res.render('layouts/teste', { teste: data });
   });
 });
-const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'teste34',
+
+// 4. BUSCAR PARA EDITAR (GET)
+app.get('/teste/edit/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `SELECT * FROM teste WHERE id = ?`;
+
+    conn.query(sql, [id], (err, data) => {
+        if (err) { console.log(err); return; }
+        const registro = data[0]; 
+        res.render('upd', { registro }); 
+    });
+});
+
+// 5. SALVAR EDIÇÃO (POST)
+app.post('/teste/update', (req, res) => {
+    const { id, nome, email, senha } = req.body;
+    const sql = `UPDATE teste SET nome = ?, email = ?, senha = ? WHERE id = ?`;
+
+    conn.query(sql, [nome, email, senha, id], (err) => {
+        if (err) { console.log(err); return; }
+        res.redirect('/teste');
+    });
+});
+
+// 6. EXCLUIR (POST)
+app.post('/teste/remove/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = `DELETE FROM teste WHERE id = ?`;
+
+    conn.query(sql, [id], (err) => {
+        if (err) { console.log(err); return; }
+        res.redirect('/teste');
+    });
 });
 
 conn.connect(function(err) {
-  if (err) {
-    console.log(err);
-  }
+  if (err) { console.log(err); return; }
   console.log('Conectado ao MySQL');
-  app.listen(3000);
+  app.listen(3000, () => console.log("Servidor em http://localhost:3000"));
 });
